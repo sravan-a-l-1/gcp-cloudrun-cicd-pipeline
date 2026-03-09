ba3b7b4895a9ccefeab45197f21571bc7ecd21b8
Sravan <sravanaal018@example.com>
2026-03-09 11:57:17 +0000

    test automatic deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cloud Run CI/CD pipeline working 🚀"
+    return "Cloud Run CI/CD Deployed Automatically"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
